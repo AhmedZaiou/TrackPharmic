f687ec24cd1a5bfd39fe00ac14e901a69d0dad2e
Ahmed Zaiou <ahmedzaiou@mac.home>
2025-07-21 19:12:48 +0200

    wip

--- test.py
+++
@@ -1,8 +0,0 @@
-
-def generate_barcode(barcode_data):  
-    barcode_format = barcode.get_barcode_class('EAN13')  # Barcode format (EAN13 in this case)
-    barcode_instance = barcode_format(barcode_data, writer=ImageWriter())
-
-    # Save the barcode as an image file
-    barcode_image = barcode_instance.render()  # Generate the barcode image
-    barcode_image.save("barcode.png")  # Save the barcode as PNG
